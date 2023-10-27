dc93e3ebf18578d0a92c42b34f3423bf3cc0fc3e
Giovanni Scarpetta <gioovanniscarpetta@gmail.com>
2023-10-27 00:00:20 -0500

    Estructura inicial del backend de chandilove

---
+++ src/main/java/com/chandilove/appchl/ChandiloveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chandilove.appchl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChandiloveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChandiloveBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chandilove/appchl/ChandiloveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandilove.appchl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChandiloveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
